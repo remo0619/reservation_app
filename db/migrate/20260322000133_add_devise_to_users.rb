ded2ec881376df38b66074927fde0d2294496ac1
remo0619 <ichinohe@aomori-innovators.com>
2026-03-22 09:08:29 +0900

    deviseでモデル作り直し

--- db/migrate/20260322000133_add_devise_to_users.rb
+++ db/migrate/20260322000133_add_devise_to_users.rb
@@ -2,10 +2,11 @@
 
 class AddDeviseToUsers < ActiveRecord::Migration[7.2]
   def self.up
-    change_table :users do |t|
+    create_table :users do |t|
       ## Database authenticatable
-      ##t.string :email,              null: false, default: ""
+      t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name,               null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_21_115851) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_22_000133) do
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.string "bio"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
