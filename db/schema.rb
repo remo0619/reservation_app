cee798d1ccaec34f52195b560cbda51a01d3bf34
remo0619 <ichinohe@aomori-innovators.com>
2026-03-27 07:18:35 +0900

    DBカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_22_000133) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_26_221545) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_22_000133) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "image"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
