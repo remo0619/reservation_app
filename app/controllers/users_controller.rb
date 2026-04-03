d7498d5f13efecaf333f90c4c8571e197b3d6c2a
remo0619 <ichinohe@aomori-innovators.com>
2026-04-03 22:59:48 +0900

    プロフ画像削除機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.avatar.attached(params[:user][:avatar]) if @user.avatar.blank?
-    if @user.update(params.require(:user).permit(:name, :email, :bio, :avatar))
+    if @user.update(params.require(:user).permit(:name, :email, :bio, :avatar, :remove_avatar))
       flash[:notice] = "ユーザー情報を更新しました"
       redirect_to user_path(current_user)
     else
