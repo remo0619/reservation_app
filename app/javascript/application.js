a0cd6ee4e8d0c6faa31eca414d14ed236bbbc7f1
remo0619 <ichinohe@aomori-innovators.com>
2026-04-03 15:53:27 +0900

    画像プレビュー実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// 編集画面のプレビュー機能
+window.previewImage = function(input) {
+  if (input.files && input.files[0]) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      const preview = document.getElementById('preview');
+      preview.src = e.target.result;
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
+}
